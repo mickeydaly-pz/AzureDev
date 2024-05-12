96e23edc14f1f578ba81f5f35941e01ba0573e54
Mickey Daly <mickey@stvital.io>
2024-05-12 11:10:43 -0500

    test

--- Install-Domain.ps1
+++ Install-Domain.ps1
@@ -3,17 +3,16 @@ param(
     [securestring]$dsrmPassword,
     [securestring]$localPassword,
     [string]$domainName,
-    [string]$username
+    [string]$username,
+    [boolean]$dnsExists
 )
 Get-Disk | Where-Object partitionstyle -eq 'raw' | Initialize-Disk -PassThru | New-Partition -DriveLetter E -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel 'data' -Confirm:$false
 Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools
 
 Restart-NetAdapter -Name "Ethernet"
 
-$dnsExists = Resolve-DnsName -Name $domainName
-
 if ($dnsExists) {
-    
+    Install-ADDSDomainController -Credential (New-Object System.Management.Automation.PSCredential ($username, $localPassword)) -DatabasePath 'E:\NTDS' -LogPath 'E:\Logs' -SysvolPath 'E:\SYSVOL' -DomainName $domainName -InstallDNS -SafeModeAdministratorPassword $dsrmPassword -Confirm:$false
 } else {
     Install-ADDSForest -DomainName $domainName -DatabasePath 'E:\NTDS' -LogPath 'E:\Logs' -SysvolPath 'E:\SYSVOL' -SafeModeAdministratorPassword $dsrmPassword -InstallDNS -Confirm:$false
 }
