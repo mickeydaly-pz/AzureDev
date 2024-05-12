9b9b1633bf075ceb0b916d00a5eabae47ef235aa
Mickey Daly <mickey@stvital.io>
2024-05-12 16:04:36 -0500

    t

--- Install-Domain.ps1
+++ Install-Domain.ps1
@@ -15,7 +15,7 @@ Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools
 Restart-NetAdapter -Name "Ethernet"
 
 if ($dnsExists) {
-    Add-Content -Path $env:windir\System32\drivers\etc\hosts -Value "`n8.8.8.8`tad.pgzr.io" -Force
+    Add-Content -Path $env:windir\System32\drivers\etc\hosts -Value "`n10.5.1.4`tad.pgzr.io" -Force
     Install-ADDSDomainController -Credential (New-Object System.Management.Automation.PSCredential $username, $localPassword) -DatabasePath 'E:\NTDS' -LogPath 'E:\Logs' -SysvolPath 'E:\SYSVOL' -DomainName $domainName -InstallDNS -SafeModeAdministratorPassword $dsrmPassword -Confirm:$false
 } else {
     Install-ADDSForest -DomainName $domainName -DatabasePath 'E:\NTDS' -LogPath 'E:\Logs' -SysvolPath 'E:\SYSVOL' -SafeModeAdministratorPassword $dsrmPassword -InstallDNS -Confirm:$false
