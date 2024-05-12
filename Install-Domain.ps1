10445205312f65c40b1fade306de055e664af772
Mickey Daly <mickey@stvital.io>
2024-05-12 12:24:17 -0500

    t

--- Install-Domain.ps1
+++ Install-Domain.ps1
@@ -6,13 +6,16 @@ param(
     [string]$username,
     [boolean]$dnsExists
 )
+
+Start-Transcript -Path "C:\transcripts\transcript0.txt" -NoClobber
+
 Get-Disk | Where-Object partitionstyle -eq 'raw' | Initialize-Disk -PassThru | New-Partition -DriveLetter E -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel 'data' -Confirm:$false
 Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools
 
 Restart-NetAdapter -Name "Ethernet"
 
 if ($dnsExists) {
-    Install-ADDSDomainController -Credential (New-Object System.Management.Automation.PSCredential ($username, $localPassword)) -DatabasePath 'E:\NTDS' -LogPath 'E:\Logs' -SysvolPath 'E:\SYSVOL' -DomainName $domainName -InstallDNS -SafeModeAdministratorPassword $dsrmPassword -Confirm:$false
+    Install-ADDSDomainController -Credential (New-Object System.Management.Automation.PSCredential $username, $localPassword) -DatabasePath 'E:\NTDS' -LogPath 'E:\Logs' -SysvolPath 'E:\SYSVOL' -DomainName $domainName -InstallDNS -SafeModeAdministratorPassword $dsrmPassword -Confirm:$false
 } else {
     Install-ADDSForest -DomainName $domainName -DatabasePath 'E:\NTDS' -LogPath 'E:\Logs' -SysvolPath 'E:\SYSVOL' -SafeModeAdministratorPassword $dsrmPassword -InstallDNS -Confirm:$false
 }
