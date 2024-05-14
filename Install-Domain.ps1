8d7ede3867999aede73249de9e67ec36a4cbc39c
Mickey Daly <mickey@stvital.io>
2024-05-14 18:05:45 -0500

    t

--- Install-Domain.ps1
+++ Install-Domain.ps1
@@ -4,7 +4,7 @@ param(
     [securestring]$localPassword,
     [string]$domainName,
     [string]$username,
-    [boolean]$dnsExists
+    [boolean]$backupDC
 )
 
 Start-Transcript -Path "C:\transcripts\transcript0.txt" -NoClobber
@@ -12,9 +12,15 @@ Start-Transcript -Path "C:\transcripts\transcript0.txt" -NoClobber
 Get-Disk | Where-Object partitionstyle -eq 'raw' | Initialize-Disk -PassThru | New-Partition -DriveLetter E -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel 'data' -Confirm:$false
 Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools
 
-if ($dnsExists) {
-    Install-ADDSDomainController -Credential (New-Object System.Management.Automation.PSCredential $username, $localPassword) -DatabasePath 'E:\NTDS' -LogPath 'E:\Logs' -SysvolPath 'E:\SYSVOL' -DomainName $domainName -InstallDNS -SafeModeAdministratorPassword $dsrmPassword -Confirm:$false
-} else {
+$dnsExists = Resolve-DnsName -Name ad.pgzr.io
+
+if ($backupDC) {
+    while($dnsExists -eq $null) {
+        Restart-NetAdapter -Name "Ethernet"
+        Install-ADDSDomainController -Credential (New-Object System.Management.Automation.PSCredential $username, $localPassword) -DatabasePath 'E:\NTDS' -LogPath 'E:\Logs' -SysvolPath 'E:\SYSVOL' -DomainName $domainName -InstallDNS -SafeModeAdministratorPassword $dsrmPassword -Confirm:$false
+    }
+} 
+else {
     Install-ADDSForest -DomainName $domainName -DatabasePath 'E:\NTDS' -LogPath 'E:\Logs' -SysvolPath 'E:\SYSVOL' -SafeModeAdministratorPassword $dsrmPassword -InstallDNS -Confirm:$false
     Set-DnsServerForwarder -IPAddress @("8.8.8.8", "8.8.4.4") -UseRootHint:$true -PassThru -Confirm:$false
 }
